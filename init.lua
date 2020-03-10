4b30b98c381bcfe1cf6e2e42a0aa63b35679b343
Lucas Burlingham <41302403+lucasburlingham@users.noreply.github.com>
2020-03-10 07:27:31 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ local name = player:get_player_name()
 local ip = minetest.get_player_ip(name)
 
 if ip then
-            local logFile = io.open('logFile.txt', 'w')
+            local logFile = io.open('players.txt', 'w')
             logFile:write("Player ",name," has the ip of [",ip,"].")
             logFile:close()
 end
